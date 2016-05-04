6fc9cdae2ddd472a01a3afcd0c16dc75b4a188fe
android-zhangqing <whut_zhangqing@163.com>
2016-05-04 14:09:14 +0800

    【I】集成社会化分享与登录sdk

--- tajiapp/app/src/main/java/com/zhangqing/taji/MyApplication.java
+++ tajiapp/app/src/main/java/com/zhangqing/taji/MyApplication.java
@@ -25,6 +25,7 @@ import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
 import com.nostra13.universalimageloader.utils.StorageUtils;
 import com.taobao.tae.sdk.callback.InitResultCallback;
 import com.umeng.analytics.AnalyticsConfig;
+import com.umeng.socialize.PlatformConfig;
 import com.zhangqing.taji.base.UserClass;
 import com.zhangqing.taji.database.DatabaseManager;
 
@@ -111,6 +112,13 @@ public class MyApplication extends Application {
                 Log.e("AlibabaSDK", "-------onFailure----msg:" + msg + "  code:" + code);
             }
         });
+
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        //微信 appid appsecret
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
+        //新浪微博 appkey appsecret
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        // QQ和Qzone appid appkey
     }
 
 
